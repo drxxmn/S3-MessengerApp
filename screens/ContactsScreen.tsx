f2a39161a2652aa93ad37645d92c0cc688d33378
i463444 <463444@student.fontys.nl>
2022-05-28 15:24:31 +0200

    display DB users in contact list

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,31 +1,48 @@
-import {FlatList, StyleSheet} from 'react-native';
 import * as React from 'react';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-import ContactListItem from "../components/ContactListItem";
+import {FlatList, StyleSheet} from 'react-native';
+import { API, graphqlOperation } from 'aws-amplify';
+import { View } from '../components/Themed';
+import ContactListItem from '../components/ContactListItem';
 
-import users from '../data/Users';
-
+import { listUsers }  from '../src/graphql/queries';
+import {useEffect, useState} from "react";
 
 export default function ContactsScreen() {
-  return (
-    <View style={styles.container}>
-      <FlatList
-          style={{width: '100%'}}
-          data={users}
-          renderItem={({item}) => <ContactListItem user={item} />}
-          keyExtractor={(item) => item.id}
-      />
-    </View>
-  );
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const usersData = await API.graphql(
+                    graphqlOperation(
+                        listUsers
+                    )
+                )
+                setUsers(usersData.data.listUsers.items);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        fetchUsers();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                style={{width: '100%'}}
+                data={users}
+                renderItem={({ item }) => <ContactListItem user={item} />}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
